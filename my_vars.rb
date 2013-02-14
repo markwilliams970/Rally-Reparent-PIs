3ed67d4152927b389179f1d6b3ad71f71d2ee71f
U-markw-mbp-PC\markwilliams <markwilliams@markw-mbp-PC.(none)>
2013-02-13 20:00:36 -0700

    User-specific environment variables

---
+++ my_vars.rb
@@ -0,0 +1,7 @@
+$my_username		        = 'user@company.com'
+$my_password		        = 'password'
+$my_base_url		        = "https://rally1.rallydev.com/slm"
+$my_workspace           = "My Workspace"
+$my_project             = "My Project"
+$wsapi_version          = "1.40"
+$filename               = 'parent_portfolio_items.csv'
